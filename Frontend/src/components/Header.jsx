24a612fbb5661d791d71206a4fa3c1d7ce3bf281
kowshikmeda <medakowshik8@gmail.com>
2025-03-09 16:35:35 +0530

    add header elements

--- Frontend/src/components/Header.jsx
+++ Frontend/src/components/Header.jsx
@@ -1,9 +1,34 @@
 import React from 'react'
-import '../index.css'
-export default function Header() {
+
+
+const Header = () => {
   return (
-    <div className='header'>
-      
+    <div className="w-full header ">
+      <nav className="flex items-center justify-between px-6 py-3">
+        
+        {/* Left Section - Logo */}
+        <div className="flex items-center">
+          <h1 className="text-white   text-2xl font-semibold">UrbanPulse</h1>
+        </div>
+
+        {/* Right Section - Navigation & Login */}
+        <div className="flex items-center gap-x-6">
+          <a href="#" className="text-white text-xl hover:text-gray-300">Events</a>
+          <a href="#" className="text-white text-xl hover:text-gray-300">Complaints</a>
+          <a href="#" className="text-white text-xl hover:text-gray-300">Bills</a>
+          <a href="#" className="text-white text-xl hover:text-gray-300">Appointments</a>
+
+          {/* Login Button */}
+          <button className="text-white border border-white px-4 py-1 rounded-lg hover:bg-white hover:text-blue-600 transition">
+            Login
+          </button>
+        </div>
+
+      </nav>
     </div>
-  )
+  );
 }
+
+export default Header;
+
+  
