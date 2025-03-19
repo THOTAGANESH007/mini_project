2e0acef5df27bc796aaf84f7ba9d20c17bf34958
THOTAGANESH007 <rr200007@rguktrkv.ac.in>
2025-03-19 16:23:19 +0530

    Added Port

--- Backend/app.js
+++ Backend/app.js
@@ -1,21 +1,19 @@
 require("dotenv").config();
-const express=require('express');
+const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
-const app=express();
-const port=process.env.PORT|| 8080;
+const app = express();
+const port = process.env.PORT || 8080;
 
 async function main() {
-    await mongoose.connect(process.env.MONGO_URL);
-  }
-  main()
-      .then(() => {
-           console.log("Connected to MongoDB Atlas");
-       })
-      .catch((err) => console.log(err));
-  
-  
-  
-  app.listen(port,()=>{
-      console.log("server is running");
+  await mongoose.connect(process.env.MONGO_URL);
+}
+main()
+  .then(() => {
+    console.log("Connected to MongoDB Atlas");
   })
+  .catch((err) => console.log(err));
+
+app.listen(port, () => {
+  console.log("server is running");
+});
