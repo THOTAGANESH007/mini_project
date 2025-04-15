5c740d6aa1a795e77a4038a7fa52a35a2f3d4472
THOTAGANESH007 <rr200007@rguktrkv.ac.in>
2025-04-15 12:44:05 +0530

    updated reviews & rti schema

--- Backend/models/RTI.js
+++ Backend/models/RTI.js
@@ -1,7 +1,7 @@
 import mongoose from "mongoose";
 
 const RTISchema = new mongoose.Schema({
-  userId: { type: Schema.Types.ObjectId, ref: "User", required: true },
+  userId: { type: mongoose.Schema.ObjectId, ref: "User", required: true },
   subject: { type: String, required: true },
   requestDetails: { type: String, required: true },
   status: {
--- Backend/models/Review.js
+++ Backend/models/Review.js
@@ -1,8 +1,8 @@
 import mongoose from "mongoose";
 
 const ReviewSchema = new mongoose.Schema({
-  userId: { type: Schema.Types.ObjectId, ref: "User", required: true },
-  placeId: { type: Schema.Types.ObjectId, ref: "Place", required: true },
+  userId: { type: mongoose.Schema.ObjectId, ref: "User", required: true },
+  placeId: { type: mongoose.Schema.ObjectId, ref: "Place", required: true },
   rating: { type: Number, required: true },
   review_count: { type: Number, default: 0 },
   comment: { type: String },
